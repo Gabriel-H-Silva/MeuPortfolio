844daeba78d9d06385a26d97fbb474d6afe5c45c
Gabriel-H-Silva <gabrielhosws4@gmail.com>
2024-10-01 12:05:42 -0300

    alteracoies nova

---
+++ assets/js/custom.js
@@ -0,0 +1,11 @@
+$("#showLeadMore").click(function()
+{
+    $("#showLeadMore").slideUp();
+    $("#leadMore").slideDown();
+});
+
+$("#hideLeadMore").click(function()
+{
+    $("#showLeadMore").slideDown();
+    $("#leadMore").slideUp();
+});
